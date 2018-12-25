772ef5b1a12d0d10ca4139df22f325a4655b6d6c
Eric van der Toorn <erictoorn@gmail.com>
2018-12-25 13:01:57 +0100

    basic game done!

--- lib/game/gameboard.dart
+++ lib/game/gameboard.dart
@@ -14,75 +14,147 @@ class GameBoardState extends State<GameBoard> {
   double scaleFactor = 60.0;
   double initOffsetX = 10;
   double initOffsetY = 150;
-  double holeDiameter = 30;
+  double holeDiameter = 40;
 
   @override
   void initState() {
     super.initState();
     //create our super nice holes
-    holes = new List();
+    reset();
+  }
+
+  void reset() {
     myHoles = List.generate(
         7,
-        (y) => List.generate(7, (x) {
+        (x) => List.generate(7, (y) {
               if ((x < 2 || x > 4) && (y < 2 || y > 4)) return null;
               return HoleData(x, y, true);
             }));
-
-    for (int x = 0; x < 7; x++) {
-      for (int y = 0; y < 7; y++) {
-        if ((x < 2 || x > 4) && (y < 2 || y > 4)) continue;
-        var hole = new HoleData(x, y, true);
-        if (x == 3 && y == 3) hole.hasPeg = false;
-        holes.add(hole);
-      }
-    }
+    myHoles[3][3].hasPeg = false;
   }
 
   @override
   Widget build(BuildContext context) {
-    print("Creating a new stack");
+    var list = myHoles.expand((list) => list.map(createHole)).toList();
+    list.addAll(<Widget>[
+      Positioned(
+          right: 10,
+          bottom: 10,
+          child: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                reset();
+              });
+            },
+            child: Icon(Icons.refresh, color: Colors.white),
+          ))
+    ]);
     return Container(
-//      decoration: BoxDecoration(
-//          color: Colors.grey.withOpacity(0.5),
-//          border: Border.all(color: Colors.orange)),
       child: Stack(
-        children: myHoles.expand((list) => list.map(createHole)).toList(),
+        children: list,
       ),
     );
   }
 
   Widget createHole(HoleData d) {
     if (d == null) return Container();
+//    print("Creating $d");
     return Positioned(
         left: initOffsetX + (scaleFactor * d.x),
         top: initOffsetY + (scaleFactor * d.y),
-        child: new DragTarget(
-          builder: (context, List<dynamic> candidateData, rejectedData) {
-            if (d.hasPeg) {
-              return PegWidget(holeDiameter, Colors.red, d.x, d.y);
-            }
-            return Container(
-              width: holeDiameter,
-              height: holeDiameter,
-              decoration:
-                  BoxDecoration(shape: BoxShape.circle, color: Colors.black),
-            );
-          },
-          onWillAccept: (data) {
-            print("HEYEYYE");
-            print(data);
-            return true;
-          },
-          onLeave: (data) {
-            print("WE LEFT THE SCREEN");
-          },
-          onAccept: (data) {
-            print("WE ACCEPTED>>>>");
-            setState(() {
-              d.hasPeg = true;
-            });
-          },
-        ));
+        child: d.hasPeg ? createPeg(d) : createRealHole(d));
+  }
+
+  Widget createRealHole(HoleData d) {
+    return new DragTarget(
+      builder: (context, List<dynamic> candidateData, rejectedData) {
+        print(
+            "We have the following data: $candidateData, and rejected $rejectedData");
+        return Container(
+          width: holeDiameter,
+          height: holeDiameter,
+          decoration:
+              BoxDecoration(shape: BoxShape.circle, color: Colors.black),
+        );
+      },
+      onWillAccept: (HoleData origin) {
+        bool res = canMove(origin, d);
+        print("$d will accept $origin: $res");
+        return canMove(origin, d);
+      },
+      onLeave: (HoleData origin) {
+        print("WE LEFT THE SCREEN");
+      },
+      onAccept: (HoleData origin) {
+        move(origin, d);
+      },
+    );
+  }
+
+  bool canMove(HoleData origin, HoleData destination) {
+    if (destination.hasPeg) return false;
+    if (origin.x == destination.x) {
+      if (destination.y == origin.y + 2) {
+        return this.myHoles[origin.x][origin.y + 1].hasPeg;
+      } else if (destination.y == origin.y - 2) {
+        return this.myHoles[origin.x][origin.y - 1].hasPeg;
+      }
+    } else if (origin.y == destination.y) {
+      if (destination.x == origin.x + 2) {
+        return this.myHoles[origin.x + 1][origin.y].hasPeg;
+      } else if (destination.x == origin.x - 2) {
+        return this.myHoles[origin.x - 1][origin.y].hasPeg;
+      }
+    }
+    return false;
+  }
+
+  void move(HoleData origin, HoleData destination) {
+    // at this point it has been established we can move
+    assert(canMove(origin, destination));
+
+    print("moving from $origin to $destination");
+    int midx = ((origin.x + destination.x) / 2).floor();
+    int midy = ((origin.y + destination.y) / 2).floor();
+    HoleData middle = this.myHoles[midx][midy];
+    print("Taking out $middle");
+    middle.hasPeg = false;
+    origin.hasPeg = false;
+    destination.hasPeg = true;
+    setState(() {});
+  }
+
+  Widget createPeg(HoleData d) {
+    return Draggable(
+      data: d,
+      child: Container(
+        width: this.holeDiameter,
+        height: this.holeDiameter,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.red,
+        ),
+      ),
+      childWhenDragging: Container(
+        width: this.holeDiameter,
+        height: this.holeDiameter,
+        decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.brown),
+      ),
+      feedback: Container(
+        width: this.holeDiameter + 20,
+        height: this.holeDiameter + 20,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.amber,
+        ),
+      ),
+      onDragStarted: () {
+        print("Dragging $d");
+      },
+      onDragCompleted: () {
+        print("Completed dragging $d");
+      },
+    );
   }
 }
 
@@ -95,101 +167,9 @@ class HoleData {
   bool hasPeg;
 
   HoleData(this.x, this.y, this.hasPeg);
-}
-
-class PegData {
-  int holeIndex;
-
-  PegData(this.holeIndex);
-}
-
-class PegWidget extends StatelessWidget {
-  final double diameter;
-  final double increase;
-  final Color color;
-  final Color highlightColor;
-  final int x;
-  final int y;
-
-  PegWidget(this.diameter, this.color, this.x, this.y,
-      [this.increase = 20, this.highlightColor = Colors.amber]) {
-//    this.highlightColor = this.color.withOpacity(0.75);
-  }
 
   @override
-  Widget build(BuildContext context) {
-    return Draggable(
-      data: [x, y],
-      child: Container(
-        width: diameter,
-        height: diameter,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: color,
-        ),
-      ),
-      feedback: Container(
-        width: diameter + increase,
-        height: diameter + increase,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: highlightColor,
-        ),
-      ),
-    );
-  }
-}
-
-class Peg extends StatefulWidget {
-  final Offset initPos;
-  final Color itemColor;
-
-  Peg(this.initPos, this.itemColor);
-
-  @override
-  State<StatefulWidget> createState() {
-    return new PegState();
-  }
-}
-
-class PegState extends State<Peg> {
-  Offset position = Offset(0.0, 0.0);
-
-  @override
-  void initState() {
-    super.initState();
-    position = widget.initPos;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      left: position.dx,
-      top: position.dy,
-      child: Draggable(
-        data: widget.itemColor,
-        child: Container(
-          width: 100,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.orange,
-          ),
-          height: 100,
-        ),
-        onDraggableCanceled: (velocity, offset) {
-          setState(() {
-            position = offset;
-          });
-        },
-        feedback: Container(
-          width: 120,
-          height: 120,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.orange,
-          ),
-        ),
-      ),
-    );
+  String toString() {
+    return "${hasPeg ? "Peg" : "Hole"}[$x, $y]";
   }
 }
